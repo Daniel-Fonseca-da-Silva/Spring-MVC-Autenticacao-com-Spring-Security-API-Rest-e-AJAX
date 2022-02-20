12fadad6e597f2e0f0c0f54552b5d3d961889bd3
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-20 15:17:35 -0300

    Usando JDBC Authentication

--- src/main/java/com/empresa/projeto/WebSecurityConfig.java
+++ src/main/java/com/empresa/projeto/WebSecurityConfig.java
@@ -1,18 +1,26 @@
 package com.empresa.projeto;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,16 +37,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 ).csrf().disable();
     }
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("fulano")
-                        .password("fulano")
-                        .roles("ADM")
-                        .build();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        return new InMemoryUserDetailsManager(user);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+//        UserDetails user =
+//                User.builder()
+//                        .username("sicrano")
+//                        .password(encoder.encode("sicrano"))
+//                        .roles("ADM")
+//                        .build();
+
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .passwordEncoder(encoder)
+//            .withUser(user)
+        ;
     }
+
 }
