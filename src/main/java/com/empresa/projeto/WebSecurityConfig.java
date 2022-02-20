ed4a6da3c05a094dfe6a005e1b5de8ae6db070a3
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-20 10:55:11 -0300

    Implementando o Login

---
+++ src/main/java/com/empresa/projeto/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.empresa.projeto;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("fulano")
+                        .password("fulano")
+                        .roles("ADM")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
