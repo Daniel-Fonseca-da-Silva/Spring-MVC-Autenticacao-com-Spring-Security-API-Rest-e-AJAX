91fee17a19ad6067b61dca1b335f602a99220a11
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-23 19:15:20 -0300

    Habilitando o cache

--- src/main/java/com/empresa/projeto/controller/PedidoRepository.java
+++ src/main/java/com/empresa/projeto/controller/PedidoRepository.java
@@ -2,6 +2,7 @@ package com.empresa.projeto.controller;
 
 import com.empresa.projeto.model.Pedido;
 import com.empresa.projeto.model.StatusPedido;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +15,7 @@ import java.util.List;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+    @Cacheable("pedidos")
     List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
     @Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
