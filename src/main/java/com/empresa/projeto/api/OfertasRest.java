36426de55f732e88979b88134384c489004c2357
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-26 19:09:34 -0300

    Criando ofertas

--- src/main/java/com/empresa/projeto/api/OfertasRest.java
+++ src/main/java/com/empresa/projeto/api/OfertasRest.java
@@ -6,6 +6,7 @@ import com.empresa.projeto.model.Oferta;
 import com.empresa.projeto.model.Pedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class OfertasRest {
     private PedidoRepository pedidoRepository;
 
     @PostMapping
-    public Oferta criaOferta(RequisicaoNovaOferta requisicao) {
+    public Oferta criaOferta(@RequestBody RequisicaoNovaOferta requisicao) {
         Optional<Pedido> pedidoBuscado = pedidoRepository.findById(requisicao.getPedidoId());
         if(!pedidoBuscado.isPresent()) {
             return null;
--- src/main/java/com/empresa/projeto/model/Pedido.java
+++ src/main/java/com/empresa/projeto/model/Pedido.java
@@ -91,6 +91,14 @@ public class Pedido {
         this.user = user;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public List<Oferta> getOferta() {
         return oferta;
     }
